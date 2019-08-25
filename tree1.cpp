ccd5ea297ea3b9232f0dc0513f2ac75dd72e8ae1
make2406 <45979753+make2406@users.noreply.github.com>
2019-08-25 23:56:55 +0300

    Update tree1.cpp

--- tree1.cpp
+++ tree1.cpp
@@ -20,7 +20,7 @@ public:
 class Event: public TObject
 {
 public:
-  Det a; //there are 2 detectors in the experiment
+  Det a;  
   Det b;
   ClassDef(Event, 1);
 };
@@ -32,46 +32,43 @@ void tree0 ()
   TTree * tree = new TTree ("tree", "treelibrated tree");
   Event * e = new Event;
 
-  //create a Branch
+  //создаем ветвь события
   tree-> Branch("event", &e);
 
-  //fill some events with random numbers
-
+  //заполняем события рандомными числами в цикле
   Int_t nenevt = 10000;
   for (Int_t iev=0; iev < nevent; iev++)
   {
     if (iev%1000==0) cout<<"Processing event "<<iev<<"..."<<endl;
 
     Float_t ea, eb;
-    gRandom -> Rannor(ea, eb); //"две энергии"" дают распреедление Гаусса
+    gRandom -> Rannor(ea, eb); //две энергии и распреедление Гаусса
      e -> a.e = ea;
      e -> b.e = eb;
      e -> a.t = gRandom -> Rndm();
      e -> b.t = ev -> a.t + gRandom -> Gaus (0.,.1);
-     tree -> Fill(); //заполнил дерево текущими событиями
+     tree -> Fill(); //заполняем дерево текущими событиями
   }
 
   //исследуем структуру класса
-
   tree -> StartViewer();
   //gRoot -> SetStyle("Plain");
 
-  //now we can feel us painters
+
 TCanvas *c1 = new TCanvas();
 c1  -> Divide(2,2);
 c1 -> cd(1);
 
 tree -> Draw("a.e");
-tree -> Draw("a.e", "3*(-.2<b.e && b.e <.2)", "same"); //то же самое, но с условием для энергии b;
-//масштабируется на 3
+tree -> Draw("a.e", "3*(-.2<b.e && b.e <.2)", "same");  
 c1 -> cd(2);
 
-tree -> Draw("b.e:a.e", "", "colz"); // одна энергия против другой
+tree -> Draw("b.e:a.e", "", "colz"); 
 c1 -> cd(3);
 
 
-tree -> Draw("b.t", "", "e"); //time of b with errorbars
-tree -> Draw("a.t", "", "same"); //время детектора а
+tree -> Draw("b.t", "", "e");  
+tree -> Draw("a.t", "", "same");  
 c1 -> cd(4);
 tree -> Draw("b.t:a.t");
 cout<<endl;
